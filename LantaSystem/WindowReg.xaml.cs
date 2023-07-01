41217ab0bbc87d58300acb387ae9fc292e57c6da
ffFausTtt <id.f.faust.t@gmail.com>
2023-07-01 19:22:56 +0300

    add function Reg

--- LantaSystem/WindowReg.xaml.cs
+++ LantaSystem/WindowReg.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static LantaSystem.CompatibilityOfGoods;
 
 namespace LantaSystem
 {
@@ -19,9 +21,66 @@ namespace LantaSystem
     /// </summary>
     public partial class WindowReg : Window
     {
+        Users users = new Users();
+        public User buffer;
         public WindowReg()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<User> code = DataBase.BaseModel.User.ToList();
+            var lastItem = code.Last();
+            User user = new User()
+            {
+                ID_User = lastItem.ID_User + 1,
+                Surname_User = txtSuName.Text,
+                Name_User = txtName.Text,
+                Login_User = txtLogin.Text,
+                Password_User = txtPass.Password,
+                Phone_User = txtPhon.Text,
+                Address_User = txtAdres.Text,
+                ID_Role = 2,
+            };
+            Regex checkSurName = new Regex(@"^[А-я]+$");
+            Regex checkName = new Regex(@"^[А-я]+$");
+            Regex checkLogin = new Regex(@"\b\w{2,10}\b");
+            Regex checkPassword = new Regex(@"\b\w{2,10}\b");
+            Regex checkNumberPhone = new Regex("^((\\+?7|8))\\d{10}$");
+            if (((!checkName.IsMatch(txtName.Text)) || (!checkSurName.IsMatch(txtSuName.Text)) || (!checkLogin.IsMatch(txtLogin.Text)) || (!checkPassword.IsMatch(txtPass.Password)) || (!checkNumberPhone.IsMatch(txtPhon.Text))))
+            {
+                MessageBox.Show("Ошибка ввода");
+            }
+            else
+            {
+                bool u = true;
+                foreach (var item in code)
+                {
+                    if (txtLogin.Text == item.Login_User)
+                    {
+                        u = false;
+                        MessageBox.Show("Такой пользователь уже существует");
+                        break;
+                    }
+                }
+                if (u)
+                {
+                    try
+                    {
+                        DataBase.BaseModel.User.Add(user);
+                        DataBase.BaseModel.SaveChanges();
+                        DataBase.BaseModel = new LantaSystemEntities();
+                        new WindowShop(user).Show();
+                        this.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
         }
     }
 }
