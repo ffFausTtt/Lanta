12e0a6ad109c43846a212bdf95d870190d6e2284
ffFausTtt <id.f.faust.t@gmail.com>
2023-07-02 22:51:25 +0300

    add unit test

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("23101c13-a60c-4969-a283-b1fbf103ef3d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LantaSystem;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NotAvtoriseLogin()
+        {
+            var user = DataBase.BaseModel.User.FirstOrDefault(x => x.Login_User == "" && x.Password_User == "pas1"); 
+            Assert.IsNull(user);
+        }
+
+        [TestMethod]
+        public void Avtorise()
+        {
+            var user = DataBase.BaseModel.User.FirstOrDefault(x => x.Login_User == "log1" && x.Password_User == "pas1");
+            Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public void NotAvtorisePassword()
+        {
+            var user = DataBase.BaseModel.User.FirstOrDefault(x => x.Login_User == "log1" && x.Password_User == "");
+            Assert.IsNull(user);
+        }
+
+        [TestMethod]
+        public void NotAvtorise()
+        {
+            var user = DataBase.BaseModel.User.FirstOrDefault(x => x.Login_User == "" && x.Password_User == "");
+            Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public void CreateAccount()
+        {
+            var user = DataBase.BaseModel.User.Add(new User
+            {
+                Surname_User = "Иванов",
+                Name_User = "Иван",
+                Login_User = "log4",
+                Password_User = "pas4",
+                Phone_User = "89677108688",
+                Address_User = "Нижний Новгород"
+            });
+            Assert.IsNotNull(user);
+        }
+    }
+}
